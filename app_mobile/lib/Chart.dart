a1522a3211b99f422d71f7568540d5d42e178d29
DHManh-rambo <duonghmanh123@gmail.com>
2026-03-03 08:04:01 +0700

    phần cơ bản của biểu đồ chưa làm chú thích

--- app_mobile/lib/Chart.dart
+++ app_mobile/lib/Chart.dart
@@ -1,44 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-class Chart extends StatelessWidget {
+class Chart extends StatefulWidget {
   const Chart({super.key});
+
+  @override
+  State<Chart> createState() => _ChartState();
+}
+
+class _ChartState extends State<Chart> {
+  String selectedType = "24h";
+
+  final List<double> hourlyTemp = [
+    26,
+    26,
+    25,
+    25,
+    24,
+    24,
+    25,
+    27,
+    29,
+    31,
+    32,
+    33,
+    34,
+    34,
+    33,
+    32,
+    30,
+    29,
+    28,
+    27,
+    27,
+    26,
+    26,
+    26,
+  ];
+
+  final List<double> weeklyTemp = [30, 32, 31, 29, 28, 27, 26];
+
+  final List<double> rainData = [2, 5, 0, 10, 3, 7, 1];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Weather Charts"), centerTitle: true),
+      appBar: AppBar(title: const Text("Biểu đồ thời tiết"), centerTitle: true),
 
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              "Biểu đồ thời tiết",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-
             const SizedBox(height: 20),
 
             Row(
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedType = "24h";
+                      });
+                    },
                     child: const Text("24h"),
                   ),
                 ),
                 const SizedBox(width: 10),
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedType = "7day";
+                      });
+                    },
                     child: const Text("7 ngày"),
                   ),
                 ),
                 const SizedBox(width: 10),
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selectedType = "rain";
+                      });
+                    },
                     child: const Text("Mưa"),
                   ),
                 ),
@@ -50,22 +96,50 @@ class Chart extends StatelessWidget {
             Container(
               height: 250,
               width: double.infinity,
+              padding: const EdgeInsets.all(8),
               decoration: BoxDecoration(
-                color: Colors.blue[100],
                 borderRadius: BorderRadius.circular(12),
+                border: Border.all(),
               ),
-              child: const Center(
-                child: Text(
-                  "Chart hiển thị ở đây",
-                  style: TextStyle(fontSize: 16),
+              child: LineChart(
+                LineChartData(
+                  gridData: FlGridData(show: true),
+                  borderData: FlBorderData(show: true),
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: CreateSpots(),
+                      isCurved: false,
+                      barWidth: 2,
+                      dotData: FlDotData(show: true),
+                    ),
+                  ],
                 ),
               ),
             ),
 
             const SizedBox(height: 20),
+
+            //GetDescription(),
           ],
         ),
       ),
+    );
+  }
+
+  List<FlSpot> CreateSpots() {
+    List<double> data;
+
+    if (selectedType == "24h") {
+      data = hourlyTemp;
+    } else if (selectedType == "7day") {
+      data = weeklyTemp;
+    } else {
+      data = rainData;
+    }
+
+    return List.generate(
+      data.length,
+      (index) => FlSpot((index + 1).toDouble(), data[index]),
     );
   }
 }
